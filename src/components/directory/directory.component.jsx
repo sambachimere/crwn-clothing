699b93c44970253b8b4915b3db18bfa8d492eb1f
Samba Diaw <sambachimerediaw@gmail.com>
2021-01-29 19:28:43 +0100

    WIP Routing

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -55,6 +55,7 @@ class Directory extends React.Component {
                             title={ section.title } 
                             imageUrl={ section.imageUrl }
                             size={ section.size }
+                            linkUrl={ section.linkUrl }
                         />
                     ))
                 }
