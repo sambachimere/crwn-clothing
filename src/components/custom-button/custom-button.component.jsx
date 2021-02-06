f1bd075b83dc02c0814497e2e182676b89a7a11a
Samba Diaw <sambachimerediaw@gmail.com>
2021-02-07 00:07:36 +0100

    We can group items together && we updated our shop data

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import './custom-button.styles.scss'
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => {
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => {
     return (
         <button 
-            className={`${isGoogleSignIn ? 'google-sign-in': '' } custom-button`} 
+            className={`${inverted ? 'inverted': '' } custom-button`} 
             {...otherProps}
         >
             { children }
