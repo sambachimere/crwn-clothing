088e533e98dc80eb6b43002a107af9e1ab398bcf
Samba Diaw <sambadiaw@MacBook-Pro-de-Samba.local>
2021-05-12 15:23:19 +0000

    start implementing styled-components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -11,34 +11,44 @@ import { selectCurrentUser } from './../../redux/user/user.selectors';
 
 import { ReactComponent as Logo } from './../../assets/crown.svg';
 
-import './header.styles.scss';
+import {
+    HeaderContainer,
+    LogoContainer,
+    OptionsContainer,
+    OptionLink,
+    OptionDiv
+} from "./header.styles"
 
 const Header = ({ currentUser, hidden }) => {
     return (
-        <div className="header">
-            <Link className="logo-container" to="/">
+        <HeaderContainer>
+            <LogoContainer to="/">
                 <Logo className="logo" />
-            </Link>
-            <div className="options">
-                <Link className="option" to="/shop">
+            </LogoContainer>
+            <OptionsContainer>
+                <OptionLink to="/shop">
                     SHOP
-                </Link>
-                <Link className="option" to="/contact">
+                </OptionLink>
+                <OptionLink to="/contact">
                     CONTACT
-                </Link>
+                </OptionLink>
                 {
                     currentUser ? 
-                    <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
+                    <OptionDiv onClick={() => auth.signOut()}>
+                        SIGN OUT
+                    </OptionDiv>
                     : 
-                    <Link className="option" to="/signin">SIGN IN</Link>
+                    <OptionLink to="/signin">
+                        SIGN IN
+                    </OptionLink>
                 }
                 <CartIcon />
-            </div>
+            </OptionsContainer>
             {
                 hidden ? null :
                 <CartDropdown />
             }
-        </div>
+        </HeaderContainer>
     )
 }
 
