7094a8358c68a50bc670a544571023b0ec09e939
Samba Diaw <sambadiaw@MacBook-Pro-de-Samba.local>
2021-05-16 13:38:23 +0000

    add sagas

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -3,7 +3,7 @@ import ShopActionTypes from './shop.types';
 const INITIAL_STATE = {
   collections: null,
   isFetching: false,
-  errorMessage: undefined,
+  errorMessage: undefined
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
@@ -12,19 +12,19 @@ const shopReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         isFetching: true
-      }
+      };
     case ShopActionTypes.FETCH_COLLECTIONS_SUCCESS:
       return {
         ...state,
         isFetching: false,
-        collections: action.payload,
+        collections: action.payload
       };
     case ShopActionTypes.FETCH_COLLECTIONS_FAILURE:
       return {
         ...state,
         isFetching: false,
-        errorMessage: action.payload,
-      }      
+        errorMessage: action.payload
+      };
     default:
       return state;
   }
