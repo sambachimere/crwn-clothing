2e54632f5ef371f14b331cb2bf7a1bb8329495b9
Samba Diaw <sambadiaw@MacBook-Pro-de-Samba.local>
2021-05-17 12:54:56 +0000

    implement stripe payments

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  // const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'pk_test_51Is5UnAEWBlPZmq5FWu5Bkx92ynfLhs5gKnltHKrRshqPNGixdgkPplHuSsZi9Mc7Cpn0x2H6V356AYku0qlJ1Ns00xduSM8jB';
+
+  const onToken = token => {
+    console.log(token);
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    })
+      .then(response => {
+        alert('succesful payment');
+      })
+      .catch(error => {
+        console.log('Payment Error: ', JSON.parse(error));
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        );
+      });
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
